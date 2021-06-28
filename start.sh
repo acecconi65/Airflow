355900405bf48faad18b994daeac9824dee2745e
Omar Elsayed <omar.elsayed@guest.telecomitalia.it>
2021-06-28 15:34:08 +0200

    Airflow intro

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+# initialize the database
+airflow db init
+
+airflow users create \
+    --username admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --email spiderman@superhero.org
+
+# start the web server, default port is 8080
+airflow webserver --port 8011
+#8080
